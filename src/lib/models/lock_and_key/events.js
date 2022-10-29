c30552118bce972b4b85ebb09256058e6822645e
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2022-10-29 13:06:03 -0400

    moving simple timeline events from json to js file

--- src/lib/models/lock_and_key/events.js
+++ src/lib/models/lock_and_key/events.js
@@ -22,4 +22,80 @@ export default [
       }
     ]
   },
+  {
+    "name": "COLONIZE",
+    "description": "::ACTOR:: established colonies throughout ::RANDOM_PLACE::.",
+    "effects": [
+      {
+        "type": "changeStatBy",
+        "stat": "militaryPower",
+        "value": 1
+      },
+      {
+        "type": "changeStatBy",
+        "stat": "economicPower",
+        "value": 1
+      }
+    ],
+    "conditions": [
+      {
+        "type": "statBetween",
+        "stat": "level",
+        "value": ["4", "5"]
+      }
+    ]
+  },
+  {
+    "name": "ESTABLISH_CITY",
+    "description": "::ACTOR:: founded a city.",
+    "effects": [
+      {
+        "type": "changeStatBy",
+        "stat": "economicPower",
+        "value": 1
+      },
+      {
+        "type": "changeStatBy",
+        "stat": "level",
+        "value": 1,
+        "conditions": [
+          {
+            "type": "statEquals",
+            "stat": "level",
+            "value": 2
+          }
+        ]
+      }
+    ],
+    "conditions": [
+      {
+        "type": "statBetween",
+        "stat": "level",
+        "value": ["2", "5"]
+      }
+    ]
+  },
+  {
+    "name": "REVOLT",
+    "description": "The people of ::ACTOR:: revolted against their leaders.",
+    "effects": [
+      {
+        "type": "changeStatBy",
+        "stat": "militaryPower",
+        "value": -2
+      },
+      {
+        "type": "changeStatBy",
+        "stat": "economicPower",
+        "value": -2
+      }
+    ],
+    "conditions": [
+      {
+        "type": "statBetween",
+        "stat": "level",
+        "value": ["2", "5"]
+      }
+    ]
+  },
 ]
